c41ead27bb29b985e726f4a77d8962e4439ea885
SEA786 <shams.eltaf786@gmail.com>
2024-07-19 11:39:28 +0200

    02 2. Iteration Overriding

--- com/cc/java/com/cc/java/App.java
+++ com/cc/java/com/cc/java/App.java
@@ -10,12 +10,13 @@ public class App {
 
         output(queen.doYourJob());
         output(queen.fly());
+        output ("------------------");
 
         output(worker.doYourJob());
         output(worker.fly());
-
+        output ("------------------");
         output(drone.doYourJob());
-        output(drone.fly());
+        output(drone.fly());output ("------------------");
 
     }
 
